c051f6588c1028853a233718ef682c3fa930136e
ElahCarvalho <elaine.florencio@souunit.com.br>
2024-06-12 00:20:34 -0300

    tentando de novo atualizar rosto,cabelo,maqui

---
+++ EcoBeauty/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const inputPesquisa = document.querySelector(".cabecalho__pesquisa");
+    const modalPesquisa = document.querySelector(".modal-pesquisa");
+
+    inputPesquisa.addEventListener("focus", function() {
+        modalPesquisa.style.display = "block";
+    });
+
+    inputPesquisa.addEventListener("blur", function() {
+        // Adiciona um pequeno delay para permitir clicar no modal antes que ele desapareça
+        setTimeout(function() {
+            modalPesquisa.style.display = "none";
+        }, 200);
+    });
+});
+
+// Seleciona o botão e a janela modal
+const buttonIcon = document.querySelector('.button-icon-contato');
+const modalContato = document.querySelector('.modal-icon-contato');
+
+// Adiciona um evento de clique ao botão
+buttonIcon.addEventListener('click', function() {
+    // Mostra a janela modal ao clicar no botão
+    modalContato.style.display = 'flex';
+    modalContato.style.display = 'flex-direction: column';
+});
+
+// Adiciona um evento de clique ao botão dentro da modal para fechar a modal
+const buttonCloseModal = document.querySelector('.button-modal-contato');
+buttonCloseModal.addEventListener('click', function() {
+    // Fecha a janela modal ao clicar no botão de fechar
+    modalContato.style.display = 'none';
+});
+
+const buttonIconCarrinho = document.querySelector('.button-icon-carrinho');
+const modalCarrinho = document.querySelector('.modal-carrinho');
+const buttonClose = document.querySelector('.button-modal-carrinho');
+
+buttonIconCarrinho.onclick = function() {
+    modalCarrinho.showModal()
+}
+
+buttonClose.onclick = function() {
+    modalCarrinho.close()
+}
